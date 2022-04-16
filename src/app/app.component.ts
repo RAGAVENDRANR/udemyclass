f22a0ac9afedf59841df651ec990f28e63454ee2
RAGAVENDRANR <hariragav1998@gmail.com>
2022-04-16 10:07:34 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'udemyclass';
+export class AppComponent implements OnInit{
+  
+  @HostListener('contextmenu', ['$event'])
+  onRightClick(event:any) {
+  event.preventDefault();}
+
+  constructor(){setTimeout(()=>{this.value=true,this.alerter() },2000)}
+  ngOnInit() {}
+  name=""
+  value=false
+  // to check the type of the variable = "typeof"
+  alerter(){
+    console.log(typeof this.value," = " , this.value)
+    console.log("Application loading sucess!")
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthComponent } from './authcomponents/auth.component';
+import { LoginpageComponent } from './authcomponents/loginpage/loginpage.component';
+import { SignuppageComponent } from './authcomponents/signuppage/signuppage.component';
+import { ResetpageComponent } from './authcomponents/resetpage/resetpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginpageComponent,
+    SignuppageComponent,
+    ResetpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
